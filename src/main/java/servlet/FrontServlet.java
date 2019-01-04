19e321782da244b78be6ebba1ace492cd1193dc4
aadiarbakerli <aadiarbakerli@gmail.com>
2019-01-04 09:05:12 -0500

    Started test

---
+++ src/main/java/servlet/FrontServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.servlets.DefaultServlet;
+
+public class FrontServlet extends DefaultServlet 
+{
+	private static final long serialVersionUID = 1L;
+
+    public FrontServlet() 
+    {
+    	
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		doGet(request, response);
+	}
+
+}
